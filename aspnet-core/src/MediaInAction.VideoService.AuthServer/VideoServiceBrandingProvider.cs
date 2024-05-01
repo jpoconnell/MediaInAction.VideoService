2b44ee53fe1c2e9154520658feb0e7a4e5a2c2cb
jeremiah oconnell <jpoconnell@pobox.com>
2024-04-30 21:43:03 -0500

    20240430

---
+++ aspnet-core/src/MediaInAction.VideoService.AuthServer/VideoServiceBrandingProvider.cs
@@ -0,0 +1,10 @@
+﻿using Volo.Abp.Ui.Branding;
+using Volo.Abp.DependencyInjection;
+
+namespace MediaInAction.VideoService;
+
+[Dependency(ReplaceServices = true)]
+public class VideoServiceBrandingProvider : DefaultBrandingProvider
+{
+    public override string AppName => "VideoService";
+}
